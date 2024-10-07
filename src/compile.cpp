c92230574af86fb0a5f49e0f156f4ae32e243fec
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-10-07 09:32:41 +0900

    feat(compile): support external func call

--- src/compile.cpp
+++ src/compile.cpp
@@ -1,12 +1,12 @@
 #include "compile.hpp"
 #include <algorithm>
+#include <cmath>
 #include <format>
 #include <iostream>
 #include <stack>
 #include <stdexcept>
 #include <unordered_map>
 #include <unordered_set>
-#include <variant>
 #include "cg.hpp"
 #include "xbyak.h"
 
@@ -72,28 +72,42 @@ class Compiler {
     xmm_survival_period_ = std::vector<std::optional<int32_t>>(xmm_usage_.size(), std::nullopt);
 
     // auto stack_size = std::max(operations.size() - xmm_usage_.size(), static_cast<size_t>(0));
-    size_t stack_size = 100;  // for now
+    size_t stack_size = 16;  // for now
     stack_usage_ = std::vector<std::optional<int32_t>>(stack_size, std::nullopt);
     ophash_to_location_ = std::unordered_map<int32_t, std::optional<Location>>();
   }
 
   std::vector<uint8_t> generate_code() {
+    double (*sin_ptr)(double) = std::sin;
+    void* sin_vptr = reinterpret_cast<void*>(sin_ptr);
+    double (*cos_ptr)(double) = std::cos;
+    void* cos_vptr = reinterpret_cast<void*>(cos_ptr);
+
     auto gen = Xbyak::CodeGenerator();
     gen.endbr64();
     std::cout << "endbr64" << std::endl;
+    gen.push(gen.r12);
+    std::cout << "push r12" << std::endl;
+    gen.push(gen.r13);
+    std::cout << "push r13" << std::endl;
     gen.push(gen.rbp);
     std::cout << "push rbp" << std::endl;
     gen.mov(gen.rbp, gen.rsp);
     std::cout << "mov rbp, rsp" << std::endl;
-    gen.sub(gen.rsp, 8 * stack_usage_.size());
+    size_t sub_size = 8 * stack_usage_.size() + 8;  // +8 for x86_64 ABI (16 byte alignment)
+    gen.sub(gen.rsp, sub_size);
     std::cout << std::format("sub rsp, {}", 8 * stack_usage_.size()) << std::endl;
+
+    gen.mov(gen.r12, gen.rdi);
+    std::cout << "mov r12, rdi" << std::endl;
+    gen.mov(gen.r13, gen.rsi);
+    std::cout << "mov r13, rsi" << std::endl;
+
     // start
     for (auto& op : operations_) {
       const auto& location = ophash_to_location_[op->hash_id];
-      if (location.has_value()) {  // cached
-        continue;
-      }
       auto available_xmm_idx = get_available_xmm_index(gen);
+      std::cout << std::format("[debug] available xmm idx: {}", available_xmm_idx) << std::endl;
       auto xmm_result = Xbyak::Xmm(available_xmm_idx);
       xmm_survival_period_[available_xmm_idx] = 0;
 
@@ -103,8 +117,8 @@ class Compiler {
 
       if (op->kind == OpKind::VALIABLE) {
         auto input_idx = find_index(op, inputs_);
-        gen.vmovsd(xmm_result, gen.ptr[gen.rdi + input_idx.value() * 8]);
-        std::cout << std::format("vmovsd xmm{}, [rdi + {} * 8]", available_xmm_idx,
+        gen.vmovsd(xmm_result, gen.ptr[gen.r12 + input_idx.value() * 8]);
+        std::cout << std::format("vmovsd xmm{}, [r12 + {} * 8]", available_xmm_idx,
                                  input_idx.value())
                   << std::endl;
       } else {
@@ -130,25 +144,49 @@ class Compiler {
           } else {
             throw std::runtime_error("unsupported operation");
           }
+        } else if (op->args.size() == 1) {
+          // external call
+          // move all
+          move_to_xmm0(op->args[0]->hash_id, gen);
+          for (size_t i = 1; i < xmm_usage_.size(); ++i) {
+            if (xmm_usage_[i].has_value()) {
+              bool is_result_xmm = *xmm_usage_[i] == op->hash_id;
+              if (!is_result_xmm) {
+                spill_register(i, gen);
+              }
+            }
+          }
+          if (op->kind == OpKind::SIN) {
+            gen.call(sin_vptr);
+            std::cout << std::format("call {}", sin_vptr) << std::endl;
+          } else if (op->kind == OpKind::COS) {
+            gen.call(cos_vptr);
+            std::cout << std::format("call {}", cos_vptr) << std::endl;
+          }
+          gen.vmovsd(xmm_result, Xbyak::Xmm(0));
+          std::cout << std::format("vmovsd xmm{}, xmm0", available_xmm_idx) << std::endl;
         }
       }
 
       // if op is output, store the result to memory
       auto output_idx = find_index(op, outputs_);
       if (output_idx.has_value()) {
-        gen.vmovsd(gen.ptr[gen.rsi + output_idx.value() * 8], xmm_result);
-        std::cout << std::format("vmovsd [rsi + {} * 8], xmm{}", output_idx.value(),
+        gen.vmovsd(gen.ptr[gen.r13 + output_idx.value() * 8], xmm_result);
+        std::cout << std::format("vmovsd [r13 + {} * 8], xmm{}", output_idx.value(),
                                  available_xmm_idx)
                   << std::endl;
       }
-
       update_xmm_suvival_period();
     }
 
-    gen.add(gen.rsp, 8 * stack_usage_.size());
-    std::cout << std::format("add rsp, {}", 8 * stack_usage_.size()) << std::endl;
+    gen.mov(gen.rsp, gen.rbp);
+    std::cout << "mov rsp, rbp" << std::endl;
     gen.pop(gen.rbp);
     std::cout << "pop rbp" << std::endl;
+    gen.pop(gen.r13);
+    std::cout << "pop r13" << std::endl;
+    gen.pop(gen.r12);
+    std::cout << "pop r12" << std::endl;
     gen.ret();
     std::cout << "ret" << std::endl;
 
@@ -158,12 +196,29 @@ class Compiler {
   }
 
  private:
+  void move_to_xmm0(int32_t hash_id, Xbyak::CodeGenerator& gen) {
+    auto location = ophash_to_location_[hash_id];
+    if (!location.has_value()) {
+      throw std::runtime_error("location not found. cannot move to xmm0");
+    }
+    if (location->is_xmm) {
+      if (location->idx == 0) {
+        return;  // already in xmm0
+      }
+      gen.vmovsd(Xbyak::Xmm(0), Xbyak::Xmm(location->idx));
+      std::cout << std::format("vmovsd xmm0, xmm{}", location->idx) << std::endl;
+    } else {
+      gen.vmovsd(Xbyak::Xmm(0), gen.ptr[gen.rbp - (location->idx + 1) * 8]);
+      std::cout << std::format("vmovsd xmm0, [rbp - {} * 8]", location->idx + 1) << std::endl;
+    }
+  }
+
   uint8_t get_xmm_register_idx(int32_t hash_id,
                                std::vector<uint32_t>&& dont_spill_xmm,
                                Xbyak::CodeGenerator& gen) {
     auto location = ophash_to_location_[hash_id];
     if (!location.has_value()) {
-      throw std::runtime_error("location not found");
+      throw std::runtime_error("location not found. cannot get xmm register");
     }
     if (location->is_xmm) {
       return location->idx;
@@ -210,7 +265,7 @@ class Compiler {
   }
 
   uint8_t get_available_xmm_index(Xbyak::CodeGenerator& gen) {
-    for (size_t i = 0; i < xmm_usage_.size(); ++i) {
+    for (size_t i = 1; i < xmm_usage_.size(); ++i) {
       if (xmm_usage_[i] == std::nullopt) {
         return i;
       }
