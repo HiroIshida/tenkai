eefc0b4b7c47221e03844dfd8fe0877d4eee21cf
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-10-15 22:10:07 +0900

    refactor: clean up compiler

--- src/compile.cpp
+++ src/compile.cpp
@@ -70,15 +70,6 @@ class Compiler {
            const std::vector<Operation::Ptr>& outputs,
            bool avx512) {
     operations_ = flatten(inputs, outputs);
-    std::cout << "flattend result:" << std::endl;
-    // show id, kind, operands ids
-    for (auto& op : operations_) {
-      std::cout << std::format("id: {}, kind: {}, operands: ", op->hash_id, to_string(op->kind));
-      for (auto& arg : op->args) {
-        std::cout << arg->hash_id << " ";
-      }
-      std::cout << std::endl;
-    }
     inputs_ = inputs;
     outputs_ = outputs;
     xmm_usage_ = std::vector<std::optional<int32_t>>((avx512) ? 31 : 15, std::nullopt);
@@ -90,46 +81,6 @@ class Compiler {
     ophash_to_location_ = std::unordered_map<int32_t, std::optional<Location>>();
   }
 
-  std::string visualize_state() const {
-    std::ostringstream ss;
-    const int vars_per_line = 8;
-    const int id_width = 9;  // 符号用に1文字増やす
-
-    const std::string green = "\033[32m";
-    const std::string purple = "\033[35m";
-    const std::string gray = "\033[90m";
-    const std::string reset = "\033[0m";
-
-    ss << green << "XMM Registers:\n";
-    for (size_t i = 0; i < xmm_usage_.size(); i += vars_per_line) {
-      for (size_t j = i; j < std::min(i + vars_per_line, xmm_usage_.size()); ++j) {
-        ss << green << std::format("xmm{:<2}:", j);
-        if (xmm_usage_[j].has_value()) {
-          ss << green << std::format("{:{}}", *xmm_usage_[j], id_width) << reset;
-        } else {
-          ss << green << std::string(id_width, '-') << reset;
-        }
-        ss << " ";
-      }
-      ss << "\n";
-    }
-    ss << purple << "Stack:\n";
-    for (size_t i = 0; i < stack_usage_.size(); i += vars_per_line) {
-      for (size_t j = i; j < std::min(i + vars_per_line, stack_usage_.size()); ++j) {
-        ss << purple << std::format("[{:3}]:", (j + 1) * 8);
-        if (stack_usage_[j].has_value()) {
-          ss << purple << std::format("{:{}}", *stack_usage_[j], id_width) << reset;
-        } else {
-          ss << purple << std::string(id_width, '-') << reset;
-        }
-        ss << " ";
-      }
-      ss << "\n";
-    }
-
-    return ss.str();
-  }
-
   std::vector<uint8_t> generate_code() {
     double (*sin_ptr)(double) = std::sin;
     void* sin_vptr = reinterpret_cast<void*>(sin_ptr);
@@ -138,31 +89,18 @@ class Compiler {
 
     auto gen = Xbyak::CodeGenerator(max_code_size);
     gen.endbr64();
-    std::cout << "endbr64" << std::endl;
     gen.push(gen.r12);
-    std::cout << "push r12" << std::endl;
     gen.push(gen.r13);
-    std::cout << "push r13" << std::endl;
     gen.push(gen.rbp);
-    std::cout << "push rbp" << std::endl;
     gen.mov(gen.rbp, gen.rsp);
-    std::cout << "mov rbp, rsp" << std::endl;
     size_t sub_size = 8 * stack_usage_.size() + 8;  // +8 for x86_64 ABI (16 byte alignment)
     gen.sub(gen.rsp, sub_size);
-    std::cout << std::format("sub rsp, {}", 8 * stack_usage_.size()) << std::endl;
-
     gen.mov(gen.r12, gen.rdi);
-    std::cout << "mov r12, rdi" << std::endl;
     gen.mov(gen.r13, gen.rsi);
-    std::cout << "mov r13, rsi" << std::endl;
 
-    // start
     for (auto& op : operations_) {
-      std::cout << visualize_state();
-
       const auto& location = ophash_to_location_[op->hash_id];
       auto available_xmm_idx = get_available_xmm_index(gen);
-      std::cout << std::format("[debug] available xmm idx: {}", available_xmm_idx) << std::endl;
       auto xmm_result = Xbyak::Xmm(available_xmm_idx);
       xmm_survival_period_[available_xmm_idx] = 0;
 
@@ -173,9 +111,6 @@ class Compiler {
       if (op->kind == OpKind::VALIABLE) {
         auto input_idx = find_index(op, inputs_);
         gen.vmovsd(xmm_result, gen.ptr[gen.r12 + input_idx.value() * 8]);
-        std::cout << std::format("vmovsd xmm{}, [r12 + {} * 8]", available_xmm_idx,
-                                 input_idx.value())
-                  << std::endl;
       } else {
         if (op->args.size() == 2) {
           auto xmm_idx_left = get_xmm_register_idx(op->args[0]->hash_id, {available_xmm_idx}, gen);
@@ -183,19 +118,10 @@ class Compiler {
               get_xmm_register_idx(op->args[1]->hash_id, {available_xmm_idx, xmm_idx_left}, gen);
           if (op->kind == OpKind::ADD) {
             gen.vaddsd(xmm_result, Xbyak::Xmm(xmm_idx_left), Xbyak::Xmm(xmm_idx_right));
-            std::cout << std::format("vaddsd xmm{}, xmm{}, xmm{}", available_xmm_idx, xmm_idx_left,
-                                     xmm_idx_right)
-                      << std::endl;
           } else if (op->kind == OpKind::MUL) {
             gen.vmulsd(xmm_result, Xbyak::Xmm(xmm_idx_left), Xbyak::Xmm(xmm_idx_right));
-            std::cout << std::format("vmulsd xmm{}, xmm{}, xmm{}", available_xmm_idx, xmm_idx_left,
-                                     xmm_idx_right)
-                      << std::endl;
           } else if (op->kind == OpKind::SUB) {
             gen.vsubsd(xmm_result, Xbyak::Xmm(xmm_idx_left), Xbyak::Xmm(xmm_idx_right));
-            std::cout << std::format("vsubsd xmm{}, xmm{}, xmm{}", available_xmm_idx, xmm_idx_left,
-                                     xmm_idx_right)
-                      << std::endl;
           } else {
             throw std::runtime_error("unsupported operation");
           }
@@ -213,13 +139,10 @@ class Compiler {
           }
           if (op->kind == OpKind::SIN) {
             gen.call(sin_vptr);
-            std::cout << std::format("call {}", sin_vptr) << std::endl;
           } else if (op->kind == OpKind::COS) {
             gen.call(cos_vptr);
-            std::cout << std::format("call {}", cos_vptr) << std::endl;
           }
           gen.vmovsd(xmm_result, Xbyak::Xmm(0));
-          std::cout << std::format("vmovsd xmm{}, xmm0", available_xmm_idx) << std::endl;
         }
       }
 
@@ -227,23 +150,15 @@ class Compiler {
       auto output_idx = find_index(op, outputs_);
       if (output_idx.has_value()) {
         gen.vmovsd(gen.ptr[gen.r13 + output_idx.value() * 8], xmm_result);
-        std::cout << std::format("vmovsd [r13 + {} * 8], xmm{}", output_idx.value(),
-                                 available_xmm_idx)
-                  << std::endl;
       }
       update_xmm_suvival_period();
     }
 
     gen.mov(gen.rsp, gen.rbp);
-    std::cout << "mov rsp, rbp" << std::endl;
     gen.pop(gen.rbp);
-    std::cout << "pop rbp" << std::endl;
     gen.pop(gen.r13);
-    std::cout << "pop r13" << std::endl;
     gen.pop(gen.r12);
-    std::cout << "pop r12" << std::endl;
     gen.ret();
-    std::cout << "ret" << std::endl;
 
     auto code = std::vector<uint8_t>(gen.getSize());
     std::copy(gen.getCode(), gen.getCode() + gen.getSize(), code.begin());
@@ -261,10 +176,8 @@ class Compiler {
         return;  // already in xmm0
       }
       gen.vmovsd(Xbyak::Xmm(0), Xbyak::Xmm(location->idx));
-      std::cout << std::format("vmovsd xmm0, xmm{}", location->idx) << std::endl;
     } else {
       gen.vmovsd(Xbyak::Xmm(0), gen.ptr[gen.rbp - (location->idx + 1) * 8]);
-      std::cout << std::format("vmovsd xmm0, [rbp - {} * 8]", location->idx + 1) << std::endl;
     }
   }
 
@@ -281,8 +194,6 @@ class Compiler {
     auto xmm_spill = find_most_unused_xmm_idx(std::move(dont_spill_xmm));
     spill_register(xmm_spill, gen);
     gen.vmovsd(Xbyak::Xmm(xmm_spill), gen.ptr[gen.rbp - (location->idx + 1) * 8]);
-    std::cout << std::format("vmovsd xmm{}, [rbp - {} * 8]", xmm_spill, location->idx + 1)
-              << std::endl;
     return xmm_spill;
   }
 
@@ -335,7 +246,6 @@ class Compiler {
     auto hash_id = *xmm_usage_[xmm_idx];
     auto stack_idx = get_avaialble_stack_index();
     gen.vmovsd(gen.ptr[gen.rbp - (stack_idx + 1) * 8], Xbyak::Xmm(xmm_idx));
-    std::cout << std::format("vmovsd [rbp - {} * 8], xmm{}", stack_idx + 1, xmm_idx) << std::endl;
 
     // update internal state
     xmm_usage_[xmm_idx] = std::nullopt;
@@ -363,7 +273,6 @@ std::vector<uint8_t> get_instructions(const std::vector<Operation::Ptr>& inputs,
 JitFunc<double> compile(const std::vector<Operation::Ptr>& inputs,
                         const std::vector<Operation::Ptr>& outputs) {
   auto code = get_instructions(inputs, outputs);
-  std::cout << std::format("code size: {}", code.size()) << std::endl;
   void* mem = mmap(NULL, max_code_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   uint8_t* instruction = static_cast<uint8_t*>(mem);
   std::memcpy(instruction, code.data(), code.size());
